28f3391836229e8905baef80a443212d8d974bae
hrom_90@hotmail.com <71078401+Nickolas990@users.noreply.github.com>
2022-03-16 23:23:11 +0300

    Realized Business Logic. Cryption, Encryption and Brute Force are working. Reducing useless code.

--- src/com.java.javarush/cryptography/BruteForceEncryptor.java
+++ src/com.java.javarush/cryptography/BruteForceEncryptor.java
@@ -4,27 +4,20 @@ import cryptography.dictionaries.CircleOfLetters;
 
 import java.io.*;
 import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.Set;
 
-public class BruteForceEncryptor {
+public class BruteForceEncryptor extends EnCrypt {
     private char[] alphabet;
     private Path crypted;
-    private Path encrypted;
 
-    public BruteForceEncryptor(Path crypted, Path encrypted) {
+    public BruteForceEncryptor(Path crypted) {
         this.alphabet = new CircleOfLetters().getAlphabet();
         this.crypted = crypted;
-        this.encrypted = encrypted;
     }
 
     public void breaking() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(crypted)));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(encrypted)))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(crypted)))) {
             String input = reader.readLine();
             char[] charInput = input.toCharArray();
-
-
             for (int i = 0; i < alphabet.length; i++) {
                 StringBuilder sb = new StringBuilder();
                 for (char c : charInput) {
@@ -36,20 +29,4 @@ public class BruteForceEncryptor {
             e.printStackTrace();
         }
     }
-
-    private char enCrypt(char letter, int key) {
-        char cryptoLetter = letter;
-        String str = String.valueOf(letter);
-        for (int i = 0; i < alphabet.length; i++) {
-            String alphabetic = String.valueOf(alphabet[i]);
-            if (str.equalsIgnoreCase(alphabetic)) {
-                if ((i - key) >= 0) {
-                    cryptoLetter = alphabet[i - key];
-                } else cryptoLetter = alphabet[i - key + alphabet.length];
-            }
-        }
-        return cryptoLetter;
-    }
-
-
 }
--- src/com.java.javarush/cryptography/EnCrypt.java
+++ src/com.java.javarush/cryptography/EnCrypt.java
@@ -9,9 +9,10 @@ import java.nio.file.Path;
 public class EnCrypt {
     private static int key = 0;
     private static char[] alphabet;
+
     public static void startStandartEncryption(Path crypted, int userKey, Path encrypted) {
         try (BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(crypted)));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(encrypted)))) {
+             BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(encrypted)))) {
             CircleOfLetters circleOfLetters = new CircleOfLetters();
             circleOfLetters.setNormalKey(userKey);
             alphabet = circleOfLetters.getAlphabet();
@@ -19,7 +20,7 @@ public class EnCrypt {
             CharBuffer cBuff = CharBuffer.allocate(1024);
             while (reader.read(cBuff) != -1) {
                 cBuff.flip();
-                while(cBuff.hasRemaining()) {
+                while (cBuff.hasRemaining()) {
                     char c = cBuff.get();
                     System.out.print(enCrypt(c, key));
                     writer.write(enCrypt(c, key));
@@ -34,7 +35,7 @@ public class EnCrypt {
 
     public static void usingBruteForce(Path crypted, Path encrypted) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            BruteForceEncryptor bruteForcer = new BruteForceEncryptor(crypted, encrypted);
+            BruteForceEncryptor bruteForcer = new BruteForceEncryptor(crypted);
             System.out.println("Here all possible variants of encrypting. Choose fit variant");
             bruteForcer.breaking();
             int userKey = Integer.parseInt(reader.readLine());
@@ -45,16 +46,15 @@ public class EnCrypt {
 
     }
 
-    private static char enCrypt (char letter, int key) {
+    static char enCrypt(char letter, int key) {
         char cryptoLetter = letter;
         String str = String.valueOf(letter);
         for (int i = 0; i < alphabet.length; i++) {
             String alphabetic = String.valueOf(alphabet[i]);
             if (str.equalsIgnoreCase(alphabetic)) {
-                if ((i - key) >= 0){
+                if ((i - key) >= 0) {
                     cryptoLetter = alphabet[i - key];
-                }
-                else cryptoLetter = alphabet[i - key + alphabet.length];
+                } else cryptoLetter = alphabet[i - key + alphabet.length];
             }
         }
         return cryptoLetter;
