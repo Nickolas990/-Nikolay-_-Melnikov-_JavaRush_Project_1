64e8728ac15c2401d9282b33d34740c590337145
hrom_90@hotmail.com <71078401+Nickolas990@users.noreply.github.com>
2022-03-23 02:55:15 +0300

    Updated StatisticEncryptor.

--- src/com/java/javarush/main/cryptography/FileChecker/FileChecker.java
+++ src/com/java/javarush/main/cryptography/FileChecker/FileChecker.java
@@ -1,7 +1,9 @@
 package com.java.javarush.main.cryptography.FileChecker;
 
 public class FileChecker {
-    private static final String WARNING = "This path is not safe for your system. If you sure in your actions, enter this path again. Or enter another.";
+    private static final String WARNING = "This path is not safe for your system.\n" +
+            "Using system directories and system files could make damage to your OS. \n " +
+            "If you sure in your actions, enter this path again. Or enter another.";
 
     public static boolean check(String filepath) {
         if (filepath.contains("windows") || filepath.contains("etc") || filepath.contains("hosts")) {
